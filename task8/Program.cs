9bc544ac3a980a2e93a3b21990a210598ce7d91c
AlmostEazy <Antosha101992@rambler.ru>
2022-09-22 00:33:53 +0300

    feat: add task8 files

---
+++ task8/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int count = 2; count <= n; count += 2) {
+    Console.WriteLine(count);
+}
+if (n <= 1)
+Console.WriteLine("По условию задачи числа должны быть больше 1");
+
+/* 2 Вариант */
+
+/* Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int count = 1; count <= n; count++) {
+    if (count % 2 == 0)
+    Console.WriteLine(count);
+} 
+if (n <= 1)
+Console.WriteLine("По условию задачи числа должны быть больше 1"); */
