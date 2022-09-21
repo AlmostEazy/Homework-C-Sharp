72fc50c93be60a7e31c6f5e6ba6896727848463a
AlmostEazy <Antosha101992@rambler.ru>
2022-09-22 00:40:10 +0300

    feat: update task8 files

--- task8/Program.cs
+++ task8/Program.cs
@@ -5,18 +5,18 @@
 
 Console.Clear();
 
-Console.Write("Enter number: ");
+/* Console.Write("Enter number: ");
 int n = int.Parse(Console.ReadLine()!);
 
 for (int count = 2; count <= n; count += 2) {
     Console.WriteLine(count);
 }
 if (n <= 1)
-Console.WriteLine("По условию задачи числа должны быть больше 1");
+Console.WriteLine("По условию задачи числа должны быть больше 1"); */
 
-/* 2 Вариант */
+/* 2 Вариант, если следовать условию задачи от 1 до N */
 
-/* Console.Write("Enter number: ");
+Console.Write("Enter number: ");
 int n = int.Parse(Console.ReadLine()!);
 
 for (int count = 1; count <= n; count++) {
@@ -24,4 +24,4 @@ for (int count = 1; count <= n; count++) {
     Console.WriteLine(count);
 } 
 if (n <= 1)
-Console.WriteLine("По условию задачи числа должны быть больше 1"); */
+Console.WriteLine("По условию задачи числа должны быть не меньше 1, а 1 - нечётное, введите число больше 1");
