c4fecff7889f7528b7055bfa0ea7390bfa1b5975
AlmostEazy <Antosha101992@rambler.ru>
2022-09-29 01:53:32 +0300

    feat: add task23 files

---
+++ task23/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n < 1) {
+    Console.WriteLine("Введите число больше 0");
+}
+
+for ( int count = 1; count <= n; count++) {
+    Console.Write(Math.Pow(count, 3)); 
+
+if (count != n) {
+    Console.Write(", ");
+} 
+      
+}
+
+
+
+/* С записью в массив */
+
+/* Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+List<double> arr = new List<double> ();             // double [] arr = new double [n];    
+
+if (n < 1) {
+    Console.WriteLine("Введите число больше 0");
+}
+
+for ( int count = 1; count <= n; count++) {         // for ( int count = 1, i = 0; count <= n; count++, i++) {
+    double result = Math.Pow(count, 3);             // double result = Math.Pow(count, 3);
+    arr.Add(result);                                // arr [i] = result;
+}                                                   
+Console.WriteLine(String.Join(", ", arr)); */
+ 
