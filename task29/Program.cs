4319ea1538d00395e5a1ad22b1f353ee9765fcb7
AlmostEazy <Antosha101992@rambler.ru>
2022-10-03 15:29:44 +0300

    feat: add task29 files

---
+++ task29/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Clear();
+
+/* Для 8 элементов */
+/* 
+int [] CreateArrey() {
+    int[] arr = new int [8];
+    return arr;
+}
+
+void FillArrey(int[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(int.MinValue, int.MaxValue);
+    }
+}
+
+void PrintArray(int[] print) {
+    Console.Write("[");
+    for (int i = 0; i < print.Length; i++) {
+        Console.Write(print[i]);
+        if ( i != print.Length - 1 ) {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] arrey = CreateArrey();
+FillArrey(arrey);
+PrintArray(arrey); */
+
+/* Для любой длины массива */
+
+int [] CreateArrey( int n ) {
+    int[] arr = new int [n];
+    return arr;
+}
+
+void FillArrey(int[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(int.MinValue, int.MaxValue);
+    }
+}
+
+void PrintArray(int[] print) {
+    Console.Write("[");
+    for (int i = 0; i < print.Length; i++) {
+        Console.Write(print[i]);
+        if ( i != print.Length - 1 ) {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите длину массива: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int[] arrey = CreateArrey(a);
+FillArrey(arrey);
+PrintArray(arrey);
