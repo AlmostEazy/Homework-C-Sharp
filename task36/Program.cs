efb181b4a8d0140a25a9cc3a597b1f67c8be5d91
AlmostEazy <Antosha101992@rambler.ru>
2022-10-05 23:19:59 +0300

    feat: add task36 files

---
+++ task36/Program.cs
@@ -0,0 +1,48 @@
+﻿
+/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+Console.Clear();
+
+int [] CreateArray(int number) {
+    int[] array = new int[number];
+    return array;
+}
+
+void FillArray(int[] array) {
+    for ( int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] print) {
+    Console.Write("[");
+    for (int i = 0; i < print.Length; i++) {
+        Console.Write(print[i]);
+        if ( i != print.Length - 1 ) {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int OddPositionElementsSum(int[] array) {
+    int count = 0;
+    for ( int i = 1; i < array.Length; i +=2) {
+        count = count + array[i];
+    }
+    return count;
+}
+
+Console.Write("Введите длину массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] arr = CreateArray(n);
+FillArray(arr);
+PrintArray(arr);
+int result = OddPositionElementsSum(arr);
+Console.WriteLine($"Сумма элементов на нечетных позициях: {result}");
